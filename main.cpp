8dcedca757968150db1110a532db16974b561951
Bearylover <bearylover37@gmail.com>
2026-09-14 18:47:55 +0900

    patch get_section_name() to check for out-of-bounds before access

--- main.cpp
+++ main.cpp
@@ -20,10 +20,11 @@ std::optional<std::string> get_section_name(const SectionHeader& section, const 
     uint32_t init_idx = section.name;
     std::string resolved_name;
     if (init_idx >= strtab.size()) return std::nullopt;
-    while (strtab[init_idx] != 0) {
+    while (strtab[init_idx] != 0 && (init_idx < strtab.size())) {
         resolved_name.push_back(strtab[init_idx]);
         init_idx++;
     }
+    if (init_idx == strtab.size()) return std::nullopt;
     return resolved_name;
 }
 
